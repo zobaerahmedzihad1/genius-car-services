230ae98ae844e612abc1ef1ee3b356bb6a28380d
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-04-23 23:28:48 +0600

    Insert Service data to the mongodb cloud database

--- genius-car-service-server/index.js
+++ genius-car-service-server/index.js
@@ -1,7 +1,8 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const res = require("express/lib/response");
+const { query } = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -22,11 +23,26 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db("geniusCar").collection("service");
 
-    app.get('/service', async (req, res) => {
+    app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    // get
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // post
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
     });
   } finally {
     //   await client.close()
